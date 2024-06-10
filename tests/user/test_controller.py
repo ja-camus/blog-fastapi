e579dd20cb5631abe3e17eef5a1d8ab3fa66add4
ja-camus <jose.camus@usach.cl>
2024-06-10 13:44:02 -0400

    Add Test for User Routes

--- tests/user/test_controller.py
+++ tests/user/test_controller.py
@@ -22,8 +22,6 @@ class TestUserController:
         created_user = create_user(db, user_data)
         assert created_user.username == user_data.username
 
-        delete_user(db, created_user.id)
-
     def test_create_user_without_username(self, db: Session):
         with pytest.raises(ValidationError):
             user_data = UserCreate(email="test@example.com", password="testpassword")
@@ -40,55 +38,29 @@ class TestUserController:
             create_user(db, user_data)
 
     # UpdateUser
-    def test_update_user(self, db: Session):
-        user_data = UserCreate(
-            username="test_user", email="test@example.com", password="testpassword"
-        )
-        created_user = create_user(db, user_data)
-
+    def test_update_user(self, db: Session, user):
         updated_data = UserUpdate(
             username="updated_user",
             email="updated@example.com",
             password="updatedpassword",
         )
-        updated_user = update_user(db, created_user.id, updated_data)
+        updated_user = update_user(db, user.id, updated_data)
 
         assert updated_user.username == updated_data.username
 
-        delete_user(db, updated_user.id)
-
-    def test_update_user_username(self, db: Session):
-        user_data = UserCreate(
-            username="test_user", email="test@example.com", password="testpassword"
-        )
-        user = create_user(db, user_data)
-
+    def test_update_user_username(self, db: Session, user):
         updated_data = UserUpdate(username="test_user_updated")
         updated_user = update_user(db, user.id, updated_data)
 
         assert updated_user.username == updated_data.username
 
-        delete_user(db, user.id)
-
-    def test_update_user_email(self, db: Session):
-        user_data = UserCreate(
-            username="test_user", email="test@example.com", password="testpassword"
-        )
-        user = create_user(db, user_data)
-
+    def test_update_user_email(self, db: Session, user):
         updated_data = UserUpdate(email="test_2@example.com")
         updated_user = update_user(db, user.id, updated_data)
 
         assert updated_user.email == updated_data.email
 
-        delete_user(db, user.id)
-
-    def test_update_user_with_invalid_email(self, db: Session):
-        user_data = UserCreate(
-            username="test_user", email="test@example.com", password="testpassword"
-        )
-        user = create_user(db, user_data)
-
+    def test_update_user_with_invalid_email(self, db: Session, user):
         with pytest.raises(ValueError):
             updated_data = UserUpdate(
                 username="updated_user",
@@ -97,52 +69,28 @@ class TestUserController:
             )
             update_user(db, user.id, updated_data)
 
-        delete_user(db, user.id)
-
-    def test_update_user_password(self, db: Session):
-        user_data = UserCreate(
-            username="test_user", email="test@example.com", password="testpassword"
-        )
-        user = create_user(db, user_data)
-
+    def test_update_user_password(self, db: Session, user):
         updated_data = UserUpdate(password="test_user_updated")
         updated_user = update_user(db, user.id, updated_data)
 
         assert check_password(updated_data.password, updated_user.password)
 
-        delete_user(db, user.id)
-
-    def test_update_user_with_invalid_password(self, db: Session):
-        user_data = UserCreate(
-            username="test_user", email="test@example.com", password="testpassword"
-        )
-        user = create_user(db, user_data)
-
+    def test_update_user_with_invalid_password(self, db: Session, user):
         with pytest.raises(ValidationError):
             updated_data = UserUpdate(password="123")
             update_user(db, user.id, updated_data)
 
-        delete_user(db, user.id)
-
     # Get
-    def test_get_user(self, db: Session):
-        user_data = UserCreate(
-            username="test_user", email="test@example.com", password="testpassword"
-        )
-        created_user = create_user(db, user_data)
+    def test_get_user(self, db: Session, user):
+        retrieved_user = get_user(db, user.id)
+        assert retrieved_user.username == user.username
 
-        retrieved_user = get_user(db, created_user.id)
-        assert retrieved_user.username == created_user.username
+    def test_get_nonexistent_user(self, db: Session):
+        retrieved_user = get_user(db, 99999)
 
-        delete_user(db, created_user.id)
+        assert retrieved_user is None
 
-    def test_get_users(self, db: Session):
-        create_user(
-            db,
-            UserCreate(
-                username="user1", email="user1@example.com", password="password1"
-            ),
-        )
+    def test_get_users(self, db: Session, user):
         create_user(
             db,
             UserCreate(
@@ -153,18 +101,10 @@ class TestUserController:
         users = get_users(db)
         assert len(users) >= 2
 
-        for user in users:
-            delete_user(db, user.id)
-
     # Delete
-    def test_delete_user(self, db: Session):
-        user_data = UserCreate(
-            username="test_user", email="test@example.com", password="testpassword"
-        )
-        created_user = create_user(db, user_data)
+    def test_delete_user(self, db: Session, user):
+        deleted = delete_user(db, user.id)
+        retrieved_user = get_user(db, user.id)
 
-        deleted = delete_user(db, created_user.id)
         assert deleted is True
-
-        retrieved_user = get_user(db, created_user.id)
         assert retrieved_user is None
--- tests/user/test_routes.py
+++ tests/user/test_routes.py
@@ -1,45 +1,107 @@
 import pytest
-
+from app.helpers.utils import check_password
 
 class TestUserRoutes:
-    def test_create_user_valid_data(self, client):
+    # CreateUser
+    def test_create_user(self, client):
         user_data = {
-            "username": "new_user",
-            "email": "new_user@example.com",
-            "password": "newpassword",
+            "username": "test_user",
+            "email": "test@example.com",
+            "password": "testpassword",
         }
         response = client.post("/users/", json=user_data)
-        assert response.status_code == 201
         created_user = response.json()
+
+        assert response.status_code == 200
         assert created_user["username"] == user_data["username"]
 
-    # Prueba para manejar la creación de un usuario con datos inválidos
-    def test_create_user_invalid_data(self, client):
-        user_data = {"email": "new_user@example.com", "password": "newpassword"}
+    def test_create_user_without_username(self, client):
+        user_data = {"email": "test@example.com", "password": "testpassword"}
         response = client.post("/users/", json=user_data)
-        assert response.status_code == 422  # Validación fallida
+
+        assert response.status_code == 422
+
+    def test_create_user_without_email(self, client):
+        user_data = {"username": "test_user", "password": "testpassword"}
+        response = client.post("/users/", json=user_data)
+
+        assert response.status_code == 422
+
+    def test_create_user_without_password(self, client):
+        user_data = {"username": "test_user", "email": "test@example.com"}
+        response = client.post("/users/", json=user_data)
+
+        assert response.status_code == 422
+
+    # UpdateUser
+    def test_update_user(self, client, user):
+        new_data = {"email": "test_updated@example.com"}
+        response = client.put(f"/users/{user.id}", json=new_data)
+        updated_user = response.json()
+
+        assert response.status_code == 200
+        assert updated_user["email"] == new_data["email"]
+
+    def test_update_user_username(self, client, user):
+        new_data = {"username": "username_updated"}
+        response = client.put(f"/users/{user.id}", json=new_data)
+        updated_user = response.json()
+
+        assert response.status_code == 200
+        assert updated_user["username"] == new_data["username"]
+
+    def test_update_user_email(self, client, user):
+        new_data = {"email": "test_updated@example.com"}
+        response = client.put(f"/users/{user.id}", json=new_data)
+        updated_user = response.json()
+
+        assert response.status_code == 200
+        assert updated_user["email"] == new_data["email"]
+
+    def test_update_user_with_invalid_email(self, client, user):
+        new_data = {"email": "test_updated.com"}
+        response = client.put(f"/users/{user.id}", json=new_data)
+        updated_user = response.json()
+
+        assert response.status_code == 422
+
+    def test_update_user_password(self, client, user):
+        new_data = {"password": "passwordupdated"}
+        response = client.put(f"/users/{user.id}", json=new_data)
+        updated_user = response.json()
+
+        assert response.status_code == 200
+        assert check_password(new_data["password"], updated_user["password"])
+
+
+    def test_update_user_with_invalid_password(self, client, user):
+        new_data = {"password": "123"}
+        response = client.put(f"/users/{user.id}", json=new_data)
+        updated_user = response.json()
+
+        assert response.status_code == 422
+
+    # GetUser
+    def test_get_user(self, client, user):
+        response = client.get(f"/users/{user.id}")
+        created_user = response.json()
+
+        assert response.status_code == 200
+        assert created_user["email"] == user.email
 
     def test_get_nonexistent_user(self, client):
         response = client.get("/users/999999")
-        assert response.status_code == 404  # No encontrado
 
-    # Prueba para actualizar un usuario
-    def test_update_user(self, client):
-        user_data = {"username": "test_user", "password": "testpassword"}
-        new_data = {"email": "test_updated@example.com"}
-        headers = '' # authenticate_user(client)
-        response = client.put("/users/", headers=headers, json=new_data)
-        assert response.status_code == 200
-        updated_user = response.json()
-        assert updated_user["email"] == new_data["email"]
+        assert response.status_code == 404
 
-    # Prueba para eliminar un usuario
-    def test_delete_user(self, client):
-        headers = '' # authenticate_user(client)
-        response = client.delete("/users/", headers=headers)
-        assert response.status_code == 204  # Sin contenido
-
-    # Prueba para obtener todos los usuarios (revisando la autenticación)
     def test_get_all_users(self, client):
         response = client.get("/users/")
-        assert response.status_code == 401  # No autorizado
+
+        assert response.status_code == 200
+
+    # DeleteUser
+    def test_delete_user(self, client, user):
+        response = client.delete(f"/users/{user.id}")
+
+        assert response.status_code == 200
+
